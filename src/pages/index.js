421dc30d0b5bb9a4b598ec5c7c46fdd5880b8562
Steph <smceff@gmail.com>
2018-12-28 01:12:04 -0500

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,8 +9,8 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>i'm trying out gatsby</p>
+    <p>and heroku</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
